83cf2e55fad2aed9de5693c8957ff8ed042ded0a
YichenCathyLiu <144185289+YichenCathyLiu@users.noreply.github.com>
2024-05-12 14:12:22 -0400

    Time: 32 ms (65.46%), Space: 12.8 MB (42.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,18 +5,5 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s)!=len(t):
-            return False
-        s_freq = {}
-        t_freq = {}
-        for char in s:
-            if char in s_freq.keys():
-                s_freq[char] = s_freq[char]+1
-            else:
-                s_freq[char] = 1
-        for char in t:
-            if char in t_freq.keys():
-                t_freq[char] = t_freq[char]+1
-            else:
-                t_freq[char] = 1
-        return s_freq == t_freq  
+        
+        return sorted(s) == sorted(t)
