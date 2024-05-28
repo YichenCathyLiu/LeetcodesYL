19cc5cafde401ff08d117bcab233f0389bf6ac5e
YichenCathyLiu <144185289+YichenCathyLiu@users.noreply.github.com>
2024-05-28 13:57:01 -0400

    Time: 84 ms (84.39%), Space: 12.3 MB (98.83%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,12 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        diary = {}
+        # solution 1: use the diary as normal two-sum
+        # diary = {}
 
-        for i in range(len(numbers)):
-            diff = target - numbers[i]
-            if diff in diary.keys():
-                return [diary[diff],i+1]
+        # for i in range(len(numbers)):
+        #     diff = target - numbers[i]
+        #     if diff in diary.keys():
+        #         return [diary[diff],i+1]
+        #     else:
+        #         diary[numbers[i]] = i+1
+
+        # solution 2 : two pointers
+        l, r = 0,len(numbers)-1
+        while l < r:
+            two_sum = numbers[l]+numbers[r]
+
+            if two_sum > target:
+                r -= 1
+            elif two_sum < target:
+                l += 1
             else:
-                diary[numbers[i]] = i+1
+                return [l+1,r+1]
         
